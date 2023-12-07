36139eb87374d095af63bfa5604f8752257e3064
sevincovskienode <sevincovsky@gmail.com>
2023-12-07 16:19:49 +0400

    created cashback btn

--- script.js
+++ script.js
@@ -1,115 +1,122 @@
-//3
+// HTML
 const incrementBtn = document.querySelector("#incrementBtn");
 const decrementBtn = document.querySelector("#decrementBtn");
 const showBtn = document.querySelector("#showBtn");
+const cashBackBtn = document.querySelector("#cashBackBtn");
 const moneyInput = document.querySelector("#moneyInput");
 const balanceEl = document.querySelector("#balanceEl");
 const list = document.querySelector("#list");
 
-
-
-
+// Bank hesabını təyin edirik
 const bankAccount = {
     balance: 0,
     limit: 1000,
     report: [],
-    date: new Date(),
 
-    deposit: function (m) {
-        if (this.balance >= this.limit || m <= 0 || !m ) {
-            alert ("Pis yaşamırsan aaa");
-            console.log("Invalid balance");
+    deposit: function (amount) {
+        if (this.balance >= this.limit || amount <= 0 || !amount) {
+            alert("Invalid deposit amount!");
             return;
         }
-        this.balance += m;
 
-        const history = {
-            type: "Cash",
-            amount: m,
-            created: this.date,
-        };
+        const cashbackAmount = this.calculateCashback(amount);
 
-        
-        this.report.push(history);
-        
-        return this.balance;
-
-    },
-
-    withdraw: function (m) {
-        const checkValid = () => {
-            if (this.balance < m) {
-                alert("Kasib,limit yoxdur");
-               
-               
-                return;
-            }
-            this.balance -= m;
-            
-            const history = {
-                type: "Withdraw",
-                amount: m,
-                created: this.date,
-            };
-
-           
-            this.report.push(history);
-
-        };
-
-
-        checkValid();
-        return this.balance;
-    },
-
-    show: function (m) {
-        const thisObj = this;
-
-        function handleMonitor() {
-            console.log(thisObj.balance);
-            console.log(thisObj.report);
+        this.balance += amount + cashbackAmount;
+        this.addTransaction("Deposit", amount);
+        if (cashbackAmount > 0) {
+            this.addTransaction("Cashback", cashbackAmount);
         }
-        handleMonitor();
+        this.updateUI();
         return this.balance;
-    }
+    },
 
+    withdraw: function (amount) {
+        if (this.balance <= 0 || amount <= 0 || !amount) {
+            alert("Invalid withdrawal amount!");
+            return;
+        }
+
+        this.balance -= amount;
+        this.addTransaction("Withdrawal", amount);
+        this.updateUI();
+        return this.balance;
+    },
+
+    show: function () {
+        console.log("Balance: " + this.balance);
+        console.log("Transaction History:", this.report);
+        return this.balance;
+    },
+
+    cashback: function (amount) {
+        if (!amount || amount <= 0) {
+            alert("Invalid cashback amount!");
+            return;
+        }
+
+        const cashbackAmount = this.calculateCashback(amount);
+        const cashbackInfo = {
+            type: "Cashback",
+            amount: cashbackAmount,
+            date: new Date()
+        };
+
+        this.balance += cashbackAmount;
+        this.report.push(cashbackInfo);
+        this.updateUI();
+        return this.balance;
+    },
+
+    addTransaction: function (type, amount) {
+        const transaction = {
+            type: type,
+            amount: amount,
+            date: new Date()
+        };
+
+        this.report.push(transaction);
+        this.updateUI();
+    },
+
+    updateUI: function () {
+        balanceEl.textContent = this.balance;
+        const newContent = this.report.map((item, index) => `
+            <tr>
+                <th scope="row">${index + 1}</th>
+                <td>${item.type}</td>
+                <td class="text-${item.type == "Deposit" || item.type == "Cashback" ? "success" : "danger"}">
+                    ${item.type == "Deposit" || item.type == "Cashback" ? "+" : "-"}${item.amount}
+                </td>
+                <td>${item.date.toLocaleString()}</td>
+            </tr>
+        `).join("");
+        list.innerHTML = newContent;
+    },
+
+    calculateCashback: function (amount) {
+        return parseFloat((amount / 100 * 3).toFixed(2));
+    }
 };
 
-
-
-
-
-//4
+// Düymələrə click hadisələrini qoşuruq
 incrementBtn.addEventListener("click", function () {
-    const value = moneyInput.value;
-    bankAccount.deposit(+value);
+    const amount = parseFloat(moneyInput.value);
+    bankAccount.deposit(amount);
     moneyInput.value = "";
 });
 
 decrementBtn.addEventListener("click", function () {
-    const value = moneyInput.value;
-    bankAccount.withdraw( + value);
+    const amount = parseFloat(moneyInput.value);
+    bankAccount.withdraw(amount);
     moneyInput.value = "";
 });
 
 showBtn.addEventListener("click", function () {
-    const result = bankAccount.show();
-
-    balanceEl.innerHTML = result;
-
-    const newContent = bankAccount.report.map (( item, index) => `
-    <tr>
-    <th scope="row">${index + 1}</th>
-    <td>${item.type}</td>
-    <td class="text-${item.type == "Cash" ? "success" : "danger"}">${item.type == "Cash"? "+" + item.amount : "-" + item.amount}</td>
-    <td>${item.created}</td>
-  </tr>
-
-    `
-    )
-    .join("");
-    list.innerHTML = newContent;
+    bankAccount.show();
 });
 
-
-
+cashBackBtn.addEventListener("click", function () {
+    const amount = parseFloat(moneyInput.value);
+    bankAccount.cashback(amount);
+    moneyInput.value = "";
+});
